a8a373d2da765fd9bb4fa3f79004f7c587dadb30
Víctor Castillo <victorcastilloaguero@gmail.com>
2021-01-11 02:24:13 +0100

    Random Stuff

--- config.h
+++ config.h
@@ -31,7 +31,7 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.1;
+static float cwscale = 1.0;
 static float chscale = 1.1;
 
 /*
@@ -96,6 +96,7 @@ static int bellvolume = 0;
 
 /* default TERM value */
 char *termname = "st-256color";
+//char *termname = "xterm-256color";
 
 /*
  * spaces per tab
